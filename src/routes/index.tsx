9889f02c6c30d9254357ee7b7c76880b82e71400
rkddbs1031 <rkddbs1031@conbridge.co.kr>
2022-05-24 17:02:31 +0900

    ADD: 기초 세팅

---
+++ src/routes/index.tsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from 'react-router-dom'
+import styles from './routes.module.scss'
+
+// import Mood from './Mood'
+// import MyPlay from './MyPlay'
+// import Search from './Search'
+// import GNB from 'routes/_shared/GNB'
+
+const App = () => {
+  return (
+    <div>gg</div>
+    // <div className={styles.appWrapper}>
+    //   <GNB />
+    //   <main className={styles.app}>
+    //     <Routes>
+    //       <Route path='/' element={<Mood />} />
+    //       <Route path='/myplay' element={<MyPlay />} />
+    //       <Route path='/search' element={<Search />} />
+    //       <Route path='*' element={<div>404</div>} />
+    //     </Routes>
+    //   </main>
+    // </div>
+  )
+}
+
+export default App
