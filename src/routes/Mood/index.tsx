db1694c45c08c1817ffd410cf907226c552830e8
rkddbs1031 <rkddbs1031@gmail.com>
2022-05-31 23:37:50 +0900

    ING: Mood 컴포넌트 작업중

--- src/routes/Mood/index.tsx
+++ src/routes/Mood/index.tsx
@@ -1,9 +1,13 @@
+import { Love } from 'assets/svgs'
 import styles from './mood.module.scss'
 
 const Mood = () => {
   return (
     <section className={styles.moodContainer}>
-      <h2>Mood</h2>
+      <h2>
+        당신의 기분과 분위기를 선택해주세요
+        <Love />
+      </h2>
     </section>
   )
 }
