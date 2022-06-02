f31f301aba5840a3088ebb7c91edda9d43217148
rkddbs1031 <rkddbs1031@gmail.com>
2022-06-03 05:37:26 +0900

    MOD: Mood component recoil 및 modal 적용

--- src/routes/Mood/index.tsx
+++ src/routes/Mood/index.tsx
@@ -1,20 +1,29 @@
 import { FormEvent } from 'react'
+import { useRecoilValue } from 'recoil'
+
 import { useState } from 'hooks'
+import { mainMoodItem, subMoodItem } from 'states'
 
 import MainMood from './MainMood'
 import SubMood from './SubMood'
-
+import Modal from 'routes/_components/Modal/ModalFrame'
 import { Love } from 'assets/svgs'
 import styles from './mood.module.scss'
 
 const Mood = () => {
   const [subMoodKey, setSubMoodKey] = useState<string>()
+  const getMainMood = useRecoilValue(mainMoodItem)
+  const getSubMood = useRecoilValue(subMoodItem)
+  const [isVerified, setIsVerified] = useState<boolean>(false)
 
   const onItemChange = (value: string) => setSubMoodKey(value)
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e?.preventDefault()
+    !(getMainMood && getSubMood) ? setIsVerified(true) : setIsVerified(false)
   }
+
+  const onClose = () => setIsVerified(false)
 
   return (
     <section className={styles.moodContainer}>
@@ -26,9 +35,12 @@ const Mood = () => {
         <MainMood onItemChange={onItemChange} />
         {subMoodKey && <SubMood moodKey={subMoodKey} />}
         <button type='submit' className={styles.moodSubmit}>
-          무드 선택
+          MOOD PLAY
         </button>
       </form>
+      <Modal isOpen={isVerified} onClose={onClose} width='400px' height='250px' text='알림창'>
+        <p className={styles.modalText}>두개의 카테고리 모두 선택해주세요!</p>
+      </Modal>
     </section>
   )
 }
