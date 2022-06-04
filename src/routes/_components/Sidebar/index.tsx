2ce482a4c8c5f963127b26e24775a0312d2fc46c
rkddbs1031 <rkddbs1031@gmail.com>
2022-06-05 06:46:24 +0900

    ADD: 모바일 버전일 때 사이드바 수정 및 햄버거 버튼 나오게 하기

--- src/routes/_components/Sidebar/index.tsx
+++ src/routes/_components/Sidebar/index.tsx
@@ -1,8 +1,9 @@
 import { NavLink } from 'react-router-dom'
 
 import { cx } from 'styles'
-import { Emotion, Music, Play, Search } from 'assets/svgs'
+import { Emotion, Music, Play, SearchIcon, Hamburger, Close } from 'assets/svgs'
 import styles from './sidebar.module.scss'
+import { useState } from 'react'
 
 const navData = [
   {
@@ -27,32 +28,44 @@ const navData = [
     id: 4,
     href: 'search',
     text: '검색',
-    icon: <Search />,
+    icon: <SearchIcon />,
   },
 ]
 
 const Sidebar = () => {
+  const [sidebarDrawer, setSidebarDrawer] = useState(false)
+
+  const handleSidebar = () => setSidebarDrawer((prev) => !prev)
+
   return (
-    <aside className={styles.container}>
-      <h2>
-        <div className={styles.icon}>
-          <Music />
-        </div>
-        Yoon Play
-      </h2>
-      <nav className={styles.nav}>
-        <ul>
-          {navData.map((item) => (
-            <li key={item.id}>
-              <NavLink to={item.href} className={({ isActive }) => cx({ [styles.isActive]: isActive })}>
-                {item.icon}
-                <span>{item.text}</span>
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </aside>
+    <>
+      <aside className={cx(styles.container, { [styles.show]: sidebarDrawer })}>
+        <h2>
+          <div className={styles.icon}>
+            <Music />
+          </div>
+          Yoon Play
+        </h2>
+        <nav className={styles.nav}>
+          <ul>
+            {navData.map((item) => (
+              <li key={item.id}>
+                <NavLink to={item.href} className={({ isActive }) => cx({ [styles.isActive]: isActive })}>
+                  {item.icon}
+                  <span>{item.text}</span>
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <button type='button' onClick={handleSidebar} className={styles.closeIcon}>
+          <Close />
+        </button>
+      </aside>
+      <button type='button' onClick={handleSidebar} className={styles.hamburger}>
+        <Hamburger />
+      </button>
+    </>
   )
 }
 export default Sidebar
